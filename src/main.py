9e71bac577cc8347aa35deabd607cd6d59fb0269
GrekaSan <Greka-San@yandex.ru>
2023-12-12 00:45:12 +0300

    rewritten main.py

--- src/main.py
+++ src/main.py
@@ -3,11 +3,15 @@ from src.hide_the_account_or_card import hide_the_account_or_card
 from src.converts_date import converts_date
 
 
-for i in open_file():
-    print(converts_date(i["date"]), i["description"])
-    if i['description'] == 'Открытие вклада':
-        print(hide_the_account_or_card(i["to"]))
-    else:
-        print(f'{hide_the_account_or_card(i["from"])} -> {hide_the_account_or_card(i["to"])}')
-    print(i["operationAmount"]["amount"], i["operationAmount"]["currency"]["name"])
-    print(' ')
+def show_result():
+    for i in open_file():
+        print(converts_date(i["date"]), i["description"])
+        if i['description'] == 'Открытие вклада':
+            print(hide_the_account_or_card(i["to"]))
+        else:
+            print(f'{hide_the_account_or_card(i["from"])} -> {hide_the_account_or_card(i["to"])}')
+        print(i["operationAmount"]["amount"], i["operationAmount"]["currency"]["name"])
+        print(' ')
+
+
+show_result()
